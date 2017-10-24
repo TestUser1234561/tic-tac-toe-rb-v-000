7fa7e18a1d394330325f105bb1d11f928dffdc4f
Morgan Fudge <mfudge92@gmail.com>
2017-10-24 15:52:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,21 @@ def valid_move?(board, index)
   end
 end
 
+def turn_count(board)
+  i = 0
+  board.each do |space|
+    if(space != " ")
+      i += 1
+    end
+  end
+  return i
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  turns % 2 == 0 ? (return "X") : (return "O")
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
